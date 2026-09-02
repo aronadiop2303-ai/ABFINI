cd427d253d493f99c8d63cb6236f90fec9fd6e4b
aronadiop2303-ai <aronadiop2303@gmail.com>
2026-09-02 23:06:36 +0200

    Exposer le pipeline Full RAG

--- rag/__init__.py
+++ rag/__init__.py
@@ -1,3 +1,10 @@
+from .pipeline import RAGResponse, answer_question
 from .retriever import RetrievedContext, build_context, retrieve_context
 
-__all__ = ["RetrievedContext", "build_context", "retrieve_context"]
+__all__ = [
+    "RAGResponse",
+    "RetrievedContext",
+    "answer_question",
+    "build_context",
+    "retrieve_context",
+]

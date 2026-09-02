6d1cd54634ba0403d3e7acd9c410a548b92dc432
aronadiop2303-ai <aronadiop2303@gmail.com>
2026-09-02 23:38:36 +0200

    Exporter le provider DeepSeek dans le module models

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,10 @@
+from .deepseek import DeepSeekProvider, DeepSeekProviderError
 from .provider import GenerationRequest, GenerationResult, TextGenerationProvider
 
-__all__ = ["GenerationRequest", "GenerationResult", "TextGenerationProvider"]
+__all__ = [
+    "DeepSeekProvider",
+    "DeepSeekProviderError",
+    "GenerationRequest",
+    "GenerationResult",
+    "TextGenerationProvider",
+]

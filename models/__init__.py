a05cd01e7bf3f078e13eca7717efb69b2e8874e8
aronadiop2303-ai <aronadiop2303@gmail.com>
2026-09-03 20:48:47 +0200

    Expose open-weight provider and model router

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,16 @@
 from .deepseek import DeepSeekProvider, DeepSeekProviderError
+from .openrouter import OpenRouterProvider, OpenRouterProviderError
 from .provider import GenerationRequest, GenerationResult, TextGenerationProvider
+from .router import ModelRouter, ModelRouterError
 
 __all__ = [
     "DeepSeekProvider",
     "DeepSeekProviderError",
+    "OpenRouterProvider",
+    "OpenRouterProviderError",
     "GenerationRequest",
     "GenerationResult",
     "TextGenerationProvider",
+    "ModelRouter",
+    "ModelRouterError",
 ]

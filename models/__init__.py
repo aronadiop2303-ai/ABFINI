39497e57830ebbba4526c918a9df8a501227343c
aronadiop2303-ai <aronadiop2303@gmail.com>
2026-09-03 21:00:59 +0200

    feat(models): export extensible provider types

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,6 @@
 from .deepseek import DeepSeekProvider, DeepSeekProviderError
+from .model_types import ModelDescriptor, ModelType, describe_provider
+from .open_compatible import OpenCompatibleProvider, OpenCompatibleProviderError
 from .openrouter import OpenRouterProvider, OpenRouterProviderError
 from .provider import GenerationRequest, GenerationResult, TextGenerationProvider
 from .router import ModelRouter, ModelRouterError
@@ -6,6 +8,11 @@ from .router import ModelRouter, ModelRouterError
 __all__ = [
     "DeepSeekProvider",
     "DeepSeekProviderError",
+    "ModelDescriptor",
+    "ModelType",
+    "describe_provider",
+    "OpenCompatibleProvider",
+    "OpenCompatibleProviderError",
     "OpenRouterProvider",
     "OpenRouterProviderError",
     "GenerationRequest",
